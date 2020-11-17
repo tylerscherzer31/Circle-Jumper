06712bc1a1a81ec1cf17918fa36a3805a319ad8b
tylerscherzer31 <30841349+tylerscherzer31@users.noreply.github.com>
2020-11-17 15:18:52 -0500

    basic concept done

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -3,8 +3,22 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float jumpForce = 10f;
     public Rigidbody2D rb;
+    public SpriteRenderer sr;
+
+    public Color colorCyan;
+    public Color colorYellow;
+    public Color colorMagenta;
+    public Color colorPink;
+
+    public string currentColor;
+    public float jumpForce = 10f;
+
+    private void Start()
+    {
+        SetRandomColor();
+
+    }
 
     void Update()
     {
@@ -13,5 +27,38 @@ public class Player : MonoBehaviour
             rb.velocity = Vector2.up * jumpForce;
 
         }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Cyan")
+        {
+
+        }
+
+    }
+
+    void SetRandomColor()
+    {
+        int index = Random.Range(0, 3);
+            switch (index)
+            {
+            case 0:
+                currentColor = "Cyan";
+                sr.color = colorCyan;
+                break;
+            case 1:
+                currentColor = "Yellow";
+                sr.color = colorYellow;
+                break;
+            case 2:
+                currentColor = "Magenta";
+                sr.color = colorMagenta;
+                break;
+            case 3:
+                currentColor = "Pink";
+                sr.color = colorPink;
+                break;
+            }
     }
 }
