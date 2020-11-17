ebd0801e396ef0d66f2aaedf1c8ca903cb9691b2
tylerscherzer31 <30841349+tylerscherzer31@users.noreply.github.com>
2020-11-17 12:35:07 -0500

    Player made & control script

---
+++ Assets/Player.cs
@@ -0,0 +1,17 @@
+﻿
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float jumpForce = 10f;
+    public Rigidbody2D rb;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump") || Input.GetMouseButtonDown(0))
+        {
+            rb.velocity = Vector2.up * jumpForce;
+
+        }
+    }
+}
